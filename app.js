453a11375f2b9ce51c07b08a20477eff63695e3d
NARNEHARIPRIYA <61201057+NARNEHARIPRIYA@users.noreply.github.com>
2020-02-28 23:27:59 +0530

    Initial commit

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
-var dice = {
-    sides: 6,
-    roll: function () {
-      var randomNumber = Math.floor(Math.random() * this.sides) + 1;
-      return randomNumber;
+﻿function generate() {
+    var num = Math.floor(Math.random() * 6) + 1;
+    console.log(num);
+    let dice=document.getElementById("number");
+    dice.innerHTML = num;
+
+    var count =1;
+    switch(count){
+        case 1: if(dice.innerHTML == 6){
+            document.getElementById("redpawn4").style.top="515px";
+            document.getElementById("redpawn4").style.left="200px";
+        }
     }
-  }
-  
-  
-  
-  //Prints dice roll to the page
-  
-  function printNumber(number) {
-    var placeholder = document.getElementById('placeholder');
-    placeholder.innerHTML = number;
-  }
-  
-  var button = document.getElementById('button');
-  
-  button.onclick = function() {
-    var result = dice.roll();
-    printNumber(result);
-  };
-  
-  
+
+}
+
+var r1=document.getElementById("redpawn1");
+var r2=document.getElementById("redpawn2");
+var r3=document.getElementById("redpawn3");
+var r4=document.getElementById("redpawn4");
+function randomMove(Color, paw) {
+    var a = document.getElementById("redpawn4");
+    switch(Color){
+        case 'red':
+             a.style.top=(515-40)+"px"; a.style.left="200px";
+    }
+}
