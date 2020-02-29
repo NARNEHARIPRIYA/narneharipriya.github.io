c8a3ad5e65070ee77eec6a7c2d9cc2d5bab3ec83
NARNEHARIPRIYA <61201057+NARNEHARIPRIYA@users.noreply.github.com>
2020-02-29 17:44:48 +0530

    Initial commit

--- app.js
+++ app.js
@@ -3,25 +3,26 @@
     console.log(num);
     let dice=document.getElementById("number");
     dice.innerHTML = num;
-
     var count =1;
+    // whare is your Array.. u not using array  i didnt understood
+    
     switch(count){
-        case 1: if(dice.innerHTML == 6){
-            document.getElementById("redpawn4").style.top="515px";
-            document.getElementById("redpawn4").style.left="200px";
+        case 1: if(num == 6){
+            console.log(num);
+            var img= '<img src="./Images/red pawn.png" id="img12" alt="red" style="width: 36px;">';
+            console.log(img);
+            arr[2].innerHTML += img;
+            // document.getElementById("redpawn4").style.top="515px";
+            // document.getElementById("redpawn4").style.left="200px";
         }
     }
-
 }
+ var c11=document.getElementsByClassName("c710");
+ console.log(c11);
+ var c2=document.getElementsByClassName("c2");
+ var c3=document.getElementsByClassName("c3");
+ var c4=document.getElementsByClassName("c4");
+ var arr=[c11,c2,c3,c4];
 
-var r1=document.getElementById("redpawn1");
-var r2=document.getElementById("redpawn2");
-var r3=document.getElementById("redpawn3");
-var r4=document.getElementById("redpawn4");
-function randomMove(Color, paw) {
-    var a = document.getElementById("redpawn4");
-    switch(Color){
-        case 'red':
-             a.style.top=(515-40)+"px"; a.style.left="200px";
-    }
-}
+
+
